acaf10595997ee121a2353956f2c5fcdc3465b43
Mohammad <m7md389@gmail.com>
2021-11-23 11:38:54 +0200

    Request to the Recipe API

---
+++ server/routes/api.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const urllib = require("urllib")
+const router = express.Router()
+
+
+
+
+
+router.get('/sanity', function(req, res) {
+    res.send("ok")
+})
+
+router.get('/recipes/:ingredient', async function(req, res) {
+    const ingredient = req.params.ingredient
+    let results
+    urllib.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function(err, data, ress) {
+        if (err)
+            throw err
+        results = JSON.parse(data.toString()).results
+
+
+
+
+
+
+        res.end()
+    })
+})
+
+
+
+module.exports = router
